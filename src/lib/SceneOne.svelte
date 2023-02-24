2058f0f1198401acc293877c10c8c078da2c483a
vaisakhkm2625 <vaisakhkm2625@gmail.com>
2023-02-24 19:57:09 +0530

    first icosahedronGeometry

---
+++ src/lib/SceneOne.svelte
@@ -0,0 +1,27 @@
+<script>
+  import {
+    PerspectiveCamera,
+    DirectionalLight,
+    AmbientLight,
+    Mesh
+  } from "@threlte/core";
+
+import {
+ MeshStandardMaterial,IcosahedronGeometry
+} from 'three'
+
+  let xpos = 0.1;
+</script>
+
+<PerspectiveCamera 
+  position={{ y:5,z:10 }} 
+  lookAt={{y:0}} >
+</PerspectiveCamera>
+
+  <DirectionalLight />
+  <AmbientLight/>
+
+<Mesh
+  geometry = { new IcosahedronGeometry()}
+  material = { new MeshStandardMaterial({color: 'seagreen'})}
+  />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,23 @@
+<script>
+  import { Canvas } from '@threlte/core';
+  import SceneOne from '$lib/SceneOne.svelte'
+</script>
 
+<section>
+  <h2> Scene one </h2>
+  <div class="canvas-wrapper">
+    <Canvas>
+      <!--<SceneOne /> -->
+      <SceneOne /> 
+
+    </Canvas>
+  </div>
+</section>
+
+  <style>
+  .canvas-wrapper{
+    position:relative;
+    border:1px solid black;
+    height: 500px;
+  }
+  </style>
