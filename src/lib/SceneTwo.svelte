bf91e95b2a4ced597bc77d24a4e617d7405330b4
Vaisakhkm2625 <68694876+Vaisakhkm2625@users.noreply.github.com>
2023-02-24 22:04:41 +0530

    Scene 2 completed, auto rotating shoe with orbial control

--- src/lib/SceneTwo.svelte
+++ src/lib/SceneTwo.svelte
@@ -1,18 +1,27 @@
 <script>
-  import {PerspectiveCamera,DirectionalLight,AmbientLight} from "@threlte/core";
+  import {PerspectiveCamera,DirectionalLight,AmbientLight,OrbitControls } from "@threlte/core";
   import { GLTF } from "@threlte/extras";
 
-  let url = "Duck.gltf";
+  const url = "https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/models/gltf/MaterialsVariantsShoe/glTF/MaterialsVariantsShoe.gltf";
 
 </script>
+
+
 
   <PerspectiveCamera
   position= {{y:5,z:10}}
   lookAt= {{y:0}}
-  />
+  >
+  
+  <OrbitControls autoRotate/>
+   </PerspectiveCamera>
+
+
 
 <DirectionalLight/>
 <AmbientLight/>
 
-<GLTF url={url} />
+
+
+<GLTF url={url} scale={30} />
 
